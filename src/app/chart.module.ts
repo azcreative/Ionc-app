c52bc644190c4f853f36939c1152d38e6e9b1e5b
azcreative <azcreativ@gmail.com>
2023-04-21 09:16:54 +1200

    3 files modified

--- src/app/chart.module.ts
+++ src/app/chart.module.ts
@@ -7,7 +7,7 @@ import { Chart } from 'chart.js';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements AfterViewInit {
-  // Importing ViewChild. We need @ViewChild decorator to get a reference to the local variable
+  // Importing ViewChild. We need @ViewChild decorator to get a reference to the local variable 
   // that we have added to the canvas element in the HTML template.
   @ViewChild('barCanvas') private barCanvas: ElementRef;
   @ViewChild('doughnutCanvas') private doughnutCanvas: ElementRef;
@@ -17,9 +17,9 @@ export class HomePage implements AfterViewInit {
   doughnutChart: any;
   lineChart: any;
 
-  constructor() {}
+  constructor() { }
 
-  // When we try to call our chart to initialize methods in ngOnInit() it shows an error nativeElement of undefined.
+  // When we try to call our chart to initialize methods in ngOnInit() it shows an error nativeElement of undefined. 
   // So, we need to call all chart methods in ngAfterViewInit() where @ViewChild and @ViewChildren will be resolved.
   ngAfterViewInit() {
     this.barChartMethod();
@@ -33,41 +33,37 @@ export class HomePage implements AfterViewInit {
       type: 'bar',
       data: {
         labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [200, 50, 30, 15, 20, 34],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
+        datasets: [{
+          label: '# of Votes',
+          data: [200, 50, 30, 15, 20, 34],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
       },
       options: {
         scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
     });
   }
 
@@ -76,27 +72,25 @@ export class HomePage implements AfterViewInit {
       type: 'doughnut',
       data: {
         labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [50, 29, 15, 10, 7],
-            backgroundColor: [
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-            ],
-            hoverBackgroundColor: [
-              '#FFCE56',
-              '#FF6384',
-              '#36A2EB',
-              '#FFCE56',
-              '#FF6384',
-            ],
-          },
-        ],
-      },
+        datasets: [{
+          label: '# of Votes',
+          data: [50, 29, 15, 10, 7],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            '#FFCE56',
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#FF6384'
+          ]
+        }]
+      }
     });
   }
 
@@ -104,19 +98,7 @@ export class HomePage implements AfterViewInit {
     this.lineChart = new Chart(this.lineCanvas.nativeElement, {
       type: 'line',
       data: {
-        labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
-          'August',
-          'September',
-          'November',
-          'December',
-        ],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'],
         datasets: [
           {
             label: 'Sell per week',
@@ -139,9 +121,9 @@ export class HomePage implements AfterViewInit {
             pointHitRadius: 10,
             data: [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15],
             spanGaps: false,
-          },
-        ],
-      },
+          }
+        ]
+      }
     });
   }
 }
